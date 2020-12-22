a8090f8b92cd1173f2a8cefc8619dc7f489912df
user <user@192.168.3.29>
2020-12-22 18:57:48 +0900

    baseのコミット漏れ

---
+++ AtCoder/src/atCoder/Base.java
@@ -0,0 +1,14 @@
+package atCoder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Base{
+  public static void main( String[] args ) throws IOException{
+    BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+    String strs = reader.readLine();
+    //配列として扱う場合
+//    String strs = reader.readLine().Split(" ");
+  }
+}
