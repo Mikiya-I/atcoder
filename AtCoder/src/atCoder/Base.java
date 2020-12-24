83b40276a37f252746d6375e971f0b0a8421f831
user <user@192.168.3.29>
2020-12-24 19:59:26 +0900

    card game for two 追加

--- AtCoder/src/atCoder/Base.java
+++ AtCoder/src/atCoder/Base.java
@@ -5,10 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Base{
+
   public static void main( String[] args ) throws IOException{
     BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
-    String strs = reader.readLine();
+
+    String str = reader.readLine();
+
     //配列として扱う場合
 //    String[] strs = reader.readLine().split(" ");
   }
+
 }
