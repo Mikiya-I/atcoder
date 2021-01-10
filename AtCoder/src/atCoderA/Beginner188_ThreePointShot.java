1396d715b114981872f4b2acd357a78ee0689a80
user <user@192.168.3.29>
2021-01-10 23:02:46 +0900

    特になし

---
+++ AtCoder/src/atCoderA/Beginner188_ThreePointShot.java
@@ -0,0 +1,24 @@
+package atCoderA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beginner188_ThreePointShot {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
+
+	    String strs[] = reader.readLine().split(" ");
+	    int X = Integer.parseInt(strs[0]);
+	    int Y = Integer.parseInt(strs[1]);
+
+	    if(Math.abs(X-Y)<3) {
+	    	System.out.println("Yes");
+	    }else {
+	    	System.out.println("No");
+	    }
+
+	}
+
+}
