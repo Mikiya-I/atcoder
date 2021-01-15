daa042952942516ec3bdde4e8aa88deb670be3b7
Mikiya-I <mikiya19961012@gmail.com>
2021-01-15 15:54:35 +0900

    ファイル名変更

--- AtCoder/src/atCoderC/Beginner057_DigitsInMultiplication.java
+++ AtCoder/src/atCoderC/Beginner057_DigitsInMultiplication.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Beginner_057DigitsInMultiplication {
+public class Beginner057_DigitsInMultiplication {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
