bf6a524c5ebbe09e1992d33a6e777038ec410973
Mikiya-I <mikiya19961012@gmail.com>
2021-01-18 21:31:39 +0900

    修正OK　最後まで行った時のケースの場合分けができていなかった

--- AtCoder/src/keyence_programmingContest2021/MexBoxes.java
+++ AtCoder/src/keyence_programmingContest2021/MexBoxes.java
@@ -36,21 +36,32 @@ public class MexBoxes {
 	    int arrayStart =0;
 	    for(int i=0;i<N;i++) {
 	    	for(int j =0;arrayStart<N ;j++,arrayStart++) {
+	    		//ボール番号が変わった時
 	    		if(a[arrayStart] != i) {
+	    			//全ての箱に入りきらなかった場合
 	    			if(j<leftBox) {
-		    			result += (leftBox-j)*i;
-		    			leftBox = j;
+		    			result += (leftBox-j)*i;//ボールが入らなかった箱の値を加算
+		    			leftBox = j;//ボールが入った箱だけ残る
 	    			}
 	    			break;
+	    			//最後まで行った時
 	    		}else if(arrayStart == N-1) {
-	    			result += leftBox*(i+1);
+	    			result += i*leftBox;
+	    			//ボールが足りなかった場合
+	    			if(j+1<leftBox) {
+	    				result += j+1;//ボールを入れた箱の分だけ加算
+	    			}
+	    			//ボールが全ての箱に入った場合
+	    			else {
+	    				result += leftBox;//残りの箱の分だけ加算(残りの箱の数値は最後のボール番号+1になるため)
+	    			}
+
 	    			leftBox = 0;
 	    		}
 	    	}
 	    	if(leftBox <=0) {
 	    		break;
 	    	}
-
 	    }
 	    System.out.println(result);
 	}
