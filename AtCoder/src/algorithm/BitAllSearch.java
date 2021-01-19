e7bcd9bc69d4f5b566bd1b8fe403d32b9b2c7e12
Mikiya-I <mikiya19961012@gmail.com>
2021-01-19 23:48:19 +0900

    bit全探索

---
+++ AtCoder/src/algorithm/BitAllSearch.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+public class BitAllSearch {
+
+	int bitAllSearch(int N) {
+
+		//最大値計算用
+	    int max = 0;
+	    //ループ回数
+	    int loop = 1<<N;
+	    //全てのケース
+	    for(int i = 0;i<loop;i++) {
+
+	    	//桁分の繰り返し
+	    	for(int j=0;j<N;j++) {
+
+	    		//ビットが立っている場所を特定
+	    		if(((i>>j) & 1)==1)  {
+
+	    			//諸々処理
+
+
+	    		}
+
+	    	}
+	    	max ++;
+	    }
+	    return max;
+	}
+
+}
