d86a2765b4ee9581dc72e35de87c71f64847ab21
Mikiya-I <mikiya19961012@gmail.com>
2021-01-23 23:01:05 +0900

    小数の計算で誤差発生　なるべく整数にする

---
+++ AtCoder/src/atCoderB/Beginner189_Alcoholic.java
@@ -0,0 +1,38 @@
+package atCoderB;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beginner189_Alcoholic {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(reader.readLine());
+
+		int N = Integer.parseInt(token.nextToken());
+		int X = Integer.parseInt(token.nextToken());
+
+		int sum = 0;
+
+		int ans;
+		for (ans = 0; ans < N;) {
+			token = new StringTokenizer(reader.readLine());
+			int v = Integer.parseInt(token.nextToken());
+			int p = Integer.parseInt(token.nextToken());
+			ans++;
+
+			sum += v*p;
+
+			if (X*100 < sum) {
+				System.out.println(ans);
+				return;
+			}
+
+		}
+		System.out.println(-1);
+
+	}
+
+}
