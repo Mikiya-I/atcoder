aa056639e33724769416b8e5ea03b90bff09ded8
Mikiya-I <mikiya19961012@gmail.com>
2021-01-26 12:53:34 +0900

    bit全探索　要復習

--- AtCoder/src/atCoder400/Beginner002_Habatsu.java
+++ AtCoder/src/atCoder400/Beginner002_Habatsu.java
@@ -14,31 +14,56 @@ public class Beginner002_Habatsu {
 		int N = Integer.parseInt(token.nextToken());
 		int M = Integer.parseInt(token.nextToken());
 
-		int[][] line = new int[N][N];
+		boolean[][] line = new boolean[N][N];
 
-		if(M==0) {
+		if (M == 0) {
 			System.out.println(1);
 			return;
 		}
 
-		for(int i=0;i<M;i++) {
+		for (int i = 0; i < M; i++) {
 			token = new StringTokenizer(reader.readLine());
-			int x = Integer.parseInt(token.nextToken());
-			int y = Integer.parseInt(token.nextToken());
+			int x = Integer.parseInt(token.nextToken())-1;
+			int y = Integer.parseInt(token.nextToken())-1;
 
-			line[x][y] = 1;
-			line[y][x] = 1;
-			line[x][x] = 1;
-			line[y][y] = 1;
+			line[x][y] = true;
+			line[y][x] = true;
+			line[x][x] = true;
+			line[y][y] = true;
 		}
 
 		int max = 1;
+		//全パターン
+		for (int i = 0, pattern = 1 << N; i < pattern; i++) {
+			//そのパターンでの人数
+			int current = Integer.bitCount(i);
+			//現時点のパターンで最大値を更新できないならスキップ(計算量削減)
+			if (current <= max)
+				continue;
+			boolean ok =true;
+			label:
+			//基準になる人
+			for (int j = 0; i>>j > 0 ; j++) {
+				//bitが立っている人
+				if(((i>>j)&1) ==1) {
+					//基準になっている人と知り合い/本人か(iより後の桁)
+					for(int k=0;i>>k > 0;k++) {
+						//bitが立っている人
+						if(((i>>k)&1)==1) {
+							//パターンと人脈に矛盾があった場合はパターンを棄却
+							if(line[j][k] != true) {
+								ok = false;
+								break label;
+							}
+						}
+					}
+				}
+			}
+			if(ok) max= current;
 
+		}
 
 		System.out.println(max);
 	}
 
-	void dfs (int[] aaa,int n){
-		if(
-	}
 }
