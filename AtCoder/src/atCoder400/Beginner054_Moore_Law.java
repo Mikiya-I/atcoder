88f59174d238b8e40617472384e19801ad6b3b61
Mikiya-I <mikiya19961012@gmail.com>
2021-02-03 22:07:15 +0900

    三分探索木

--- AtCoder/src/atCoder400/Beginner054_Moore_Law.java
+++ AtCoder/src/atCoder400/Beginner054_Moore_Law.java
@@ -9,16 +9,27 @@ public class Beginner054_Moore_Law {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		double P = Double.parseDouble(reader.readLine());
-		double high = P+1;
-		double low = -1;
-		while(high-low >0.00000001) {
-			double d = high -low;
-			if((low+high*2)/3>(low*2+high)/3) {
-				high -= d/3;
+		double high = 3000000d;
+		double low = 0;
+
+		for(int i=0;i<1000;i++){
+			//low側の1/3
+			double mid1 = low + (high - low) / 3;
+			//high側
+			double mid2 = low + (high - low) * 2 / 3;
+
+			if(cal(P,mid1)<cal(P,mid2)) {
+				high = mid2;
 			}else {
-				low +=+d/3;
+				low = mid1;
 			}
 		}
-		System.out.println(high);
+		System.out.println(cal(P,high));
+		System.out.println("");
+	}
+
+	static double cal(double P,double x) {
+		double ans = x + P/Math.pow(2, x/1.5);
+        return ans;
 	}
 }
