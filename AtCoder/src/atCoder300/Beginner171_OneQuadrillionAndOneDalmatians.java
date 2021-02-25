f05fe895ebd45145fcdc1d538c48ab6c828099ef
Mikiya-I <mikiya19961012@gmail.com>
2021-02-25 12:25:33 +0900

    名前変更

--- AtCoder/src/atCoder300/Beginner171_OneQuadrillionAndOneDalmatians.java
+++ AtCoder/src/atCoder300/Beginner171_OneQuadrillionAndOneDalmatians.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Beginner170_OneQuadrillionAndOneDalmatians {
+public class Beginner171_OneQuadrillionAndOneDalmatians {
 
 	public static void main(String[] args) throws IOException {
 
