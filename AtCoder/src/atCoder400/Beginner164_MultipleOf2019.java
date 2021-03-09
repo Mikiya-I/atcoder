17490202e568ffe65c53cb7615db07fee645e251
Mikiya-I <mikiya19961012@gmail.com>
2021-03-09 23:15:45 +0900

    合同式　途中

---
+++ AtCoder/src/atCoder400/Beginner164_MultipleOf2019.java
@@ -0,0 +1,17 @@
+package atCoder400;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Beginner164_MultipleOf2019 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+
+		
+		
+	}
+}
