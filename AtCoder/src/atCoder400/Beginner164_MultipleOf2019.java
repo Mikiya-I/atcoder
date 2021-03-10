a3732ef7f73e7b7e810060c562f830308a944b90
Mikiya-I <mikiya19961012@gmail.com>
2021-03-10 22:19:08 +0900

    mod　途中

--- AtCoder/src/atCoder400/Beginner164_MultipleOf2019.java
+++ AtCoder/src/atCoder400/Beginner164_MultipleOf2019.java
@@ -9,9 +9,24 @@ public class Beginner164_MultipleOf2019 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String str = reader.readLine();
+		char[] n = reader.readLine().toCharArray();
+		int length = n.length;
+		int [] mods = new int[2019];//余りの数の格納用(0~2018)
+		mods[0]++;//一つも取り除かない(場合は0として扱う)
 
-		
-		
+		int mod=0;
+		for(int i=length-1;0<=i;i--) {
+			mod += n[i]-'0';
+			
+			mods[mod]++;
+		}
+
+
+		long ans =0;
+		for(int  i:mods) {
+			if(i<1)
+				ans+=i*(i-1)/2;
+		}
+		System.out.println(ans);
 	}
 }
