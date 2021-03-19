ad22a86fb916c4cf6c702800a499d6813988adc3
Mikiya-I <mikiya19961012@gmail.com>
2021-03-19 21:41:35 +0900

    累積和 合同式

--- AtCoder/src/atCoder300/Beginner177_SumOfProductOfPairs.java
+++ AtCoder/src/atCoder300/Beginner177_SumOfProductOfPairs.java
@@ -11,20 +11,21 @@ public class Beginner177_SumOfProductOfPairs {
 
 		final int N = Integer.parseInt(reader.readLine());
 		String[] strs = reader.readLine().split(" ");
-		long[] sums = new long[N+1];
+		long[] sums = new long[N];
+		long[] arr = new long[N];
 		final long base = (long) (1E9+7);
 		long current =0;
 		//modの累積和
 		for(int i=0;i<N;i++) {
-			current = (current +Long.parseLong(strs[i]))%base;
-			sums[i+1]=current;
+			long now = Long.parseLong(strs[i]);
+			arr[i] = now;
+			current = (current+now)%base;
+			sums[i]=current;
 		}
 
 		long ans =0;
-		for(int i=1;i<=N;i++) {
-			long param1= (sums[i]-sums[i-1]);
-			long param2 = (sums[N]-sums[i]+base)%base;
-			ans+=(param1 *param2)%base;
+		for(int i=N-1;i>=1;i--) {
+			ans+=sums[i-1] *arr[i]%base;
 			ans %= base;
 		}
 		System.out.println(ans);
