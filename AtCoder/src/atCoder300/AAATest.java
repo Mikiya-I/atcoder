54182c528f39fa5d26683a662e3aa8663dc117f9
Mikiya-I <mikiya19961012@gmail.com>
2021-03-26 23:39:13 +0900

    テスト用

--- AtCoder/src/atCoder300/AAATest.java
+++ AtCoder/src/atCoder300/AAATest.java
@@ -1,7 +1,6 @@
 package atCoder300;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
@@ -17,19 +16,19 @@ public class AAATest {
 //			String str = String.valueOf(R);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String str = reader.readLine();
-			try {
-				long ans =test.slove(str);
-				int ans2 = test.slove2(str);
-				if(ans != ans2) {
-					System.out.println(str);
-					System.out.println(ans);
-					System.out.println(ans2);
+//			try {
+//				long ans =test.slove(str);
+//				int ans2 = test.slove2(str);
+//				if(ans != ans2) {
+//					System.out.println(str);
+//					System.out.println(ans);
+//					System.out.println(ans2);
 //					break;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println(str);
-			}
+//				}
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//				System.out.println(str);
+//			}
 //		}
 	}
 }
