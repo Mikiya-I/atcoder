b9881846d17cd40a1232cd14cf299321226b1c2c
Mikiya-I <mikiya19961012@gmail.com>
2021-04-07 23:31:51 +0900

    全探索　再帰　途中

--- AtCoder/src/atCoder300/Beginner165_ManyRequirements.java
+++ AtCoder/src/atCoder300/Beginner165_ManyRequirements.java
@@ -9,26 +9,33 @@ public class Beginner165_ManyRequirements {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		Beginner165_ManyRequirements This = new Beginner165_ManyRequirements();
-		int ans =This.slove();
+		long ans =This.slove();
 		System.out.println(ans);
 	}
 
-	public int slove() throws IOException {
+	public long slove() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = reader.readLine().split(" ");
 		final int N = Integer.parseInt(strs[0]);
 		final int M = Integer.parseInt(strs[1]);
 		final int Q = Integer.parseInt(strs[2]);
 
+		int[] A = new int[Q];
+		int[] B = new int[Q];
+		int[] C = new int[Q];
+		int[] D = new int[Q];
 		for(int i=0;i<Q ;i++) {
 			String s[] = reader.readLine().split(" ");
-			int a = Integer.parseInt(s[0]);
-			int b = Integer.parseInt(s[1]);
-			int c = Integer.parseInt(s[2]);
-			int d = Integer.parseInt(s[3]);
-		
-		
-		
+			A[i] = Integer.parseInt(s[0]);
+			B[i] = Integer.parseInt(s[1]);
+			C[i] = Integer.parseInt(s[2]);
+			D[i] = Integer.parseInt(s[3]);
 		}
+		long ans =0;
+		int array[] = new int[N];
+		int n=N;
+
+
+		return ans;
 	}
 }
