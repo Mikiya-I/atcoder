d2ece2e16b787d25c549044dfbb091e626a9f0ad
Mikiya-I <mikiya19961012@gmail.com>
2021-04-08 23:49:43 +0900

    途中

--- AtCoder/src/atCoder300/Beginner165_ManyRequirements.java
+++ AtCoder/src/atCoder300/Beginner165_ManyRequirements.java
@@ -5,37 +5,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Beginner165_ManyRequirements {
+	static long[] fac;
+	static int[] num;
+	static int max = 0;
+	static int N;
+	static int M;
+	static int Q;
+	static int[] a, b, c, d;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		Beginner165_ManyRequirements This = new Beginner165_ManyRequirements();
-		long ans =This.slove();
-		System.out.println(ans);
-	}
-
-	public long slove() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = reader.readLine().split(" ");
-		final int N = Integer.parseInt(strs[0]);
-		final int M = Integer.parseInt(strs[1]);
-		final int Q = Integer.parseInt(strs[2]);
+		N = Integer.parseInt(strs[0]);
+		M = Integer.parseInt(strs[1]);
+		Q = Integer.parseInt(strs[2]);
 
-		int[] A = new int[Q];
-		int[] B = new int[Q];
-		int[] C = new int[Q];
-		int[] D = new int[Q];
-		for(int i=0;i<Q ;i++) {
+		num = new int[N + 1];
+		a = new int[Q];
+		b = new int[Q];
+		c = new int[Q];
+		d = new int[Q];
+		for (int i = 0; i < Q; i++) {
 			String s[] = reader.readLine().split(" ");
-			A[i] = Integer.parseInt(s[0]);
-			B[i] = Integer.parseInt(s[1]);
-			C[i] = Integer.parseInt(s[2]);
-			D[i] = Integer.parseInt(s[3]);
+			a[i] = Integer.parseInt(s[0]);
+			b[i] = Integer.parseInt(s[1]);
+			c[i] = Integer.parseInt(s[2]);
+			d[i] = Integer.parseInt(s[3]);
 		}
-		long ans =0;
-		int array[] = new int[N];
-		int n=N;
 
-
-		return ans;
+		dfs(2, 1);
+		System.out.println(max);
 	}
+
+	public static void dfs(int ind, int now) {
+		if (ind > N) {
+			int sum = 0;
+			//Q個の条件を全探索
+			for (int i = 0; i < Q; i++) {
+				if (num[b[i]] - num[a[i]] == c[i])
+					sum += d[i];
+			}
+			if (sum > max)
+				max = sum;
+			return;
+		}
+
+		for (int j = now; j <= M; j++) {
+			num[ind] = j;
+			dfs(ind + 1, j);
+		}
+	}
+
 }
