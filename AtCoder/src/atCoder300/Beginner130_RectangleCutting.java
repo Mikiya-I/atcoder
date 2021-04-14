bde5b9c6cc4b97d687bf4cdbf74c2bc174db8def
Mikiya-I <mikiya19961012@gmail.com>
2021-04-14 21:23:29 +0900

    途中

---
+++ AtCoder/src/atCoder300/Beginner130_RectangleCutting.java
@@ -0,0 +1,21 @@
+package atCoder300;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beginner130_RectangleCutting {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		System.out.println();
+	}
+
+	public static  int slove() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = reader.readLine().split(" ");
+		reader.close();
+		
+		return 0;
+	}
+}
