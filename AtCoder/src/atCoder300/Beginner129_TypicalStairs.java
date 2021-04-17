11865b3daa1cfe0fd4143f27478a2086a466a197
Mikiya-I <mikiya19961012@gmail.com>
2021-04-17 23:27:02 +0900

    順列はできない

--- AtCoder/src/atCoder300/Beginner129_TypicalStairs.java
+++ AtCoder/src/atCoder300/Beginner129_TypicalStairs.java
@@ -17,33 +17,36 @@ public class Beginner129_TypicalStairs {
 		String strs[] = reader.readLine().split(" ");
 		final int N = Integer.parseInt(strs[0]);
 		final int M = Integer.parseInt(strs[1]);
-		long ans = 0;
+		long ans = 1;
 		long mod = (long) (1e9+7);
-		boolean[] step =new  boolean[N];
+		boolean[] step =new  boolean[N+1];
 		for(int i= 0;i<M;i++) {
 			step [Integer.parseInt(reader.readLine())]=true;
 		}
 		int start =0;
-		for(int i=0;i<N;i++) {
-			if(step[i]) {
+		for(int i=0;i<=N;i++) {
+			if(step[i] || i==N) {
 				//2段連続していたら0
 				if(i-start==0) {
 					return 0;
 				}
 				//対象の段数
-				int stepCount = (i-start);
+				int stepCount = i==N?i-start:i-start-1;
 				//2段の最大値
 				int twoCount = stepCount/2;
+				long sectionPattern=0;
 				// 全部1段ずつのパターン
-				ans++;
-				for(int j= 1;i<twoCount;i++) {
-					ans +=  modFactorial(stepCount-j,mod)/(modFactorial(,mod)*modFactorial(, mod));
-					ans %= mod;
+				sectionPattern++;
+				for(int j= 1;j<=twoCount;j++) {
+					sectionPattern +=  modFactorial(stepCount-j,mod)/(modFactorial(j,mod)*modFactorial(stepCount-j-j, mod));
+					sectionPattern %= mod;
 				}
+				ans *= sectionPattern;
+				ans%=mod;
 				start = i+1;
 			}
 		}
-		return 1;
+		return ans;
 	}
 	
 	public static long modFactorial(int n,long mod) {
