1df046cd6305d870e2c293b26def51e4518cc422
Mikiya-I <mikiya19961012@gmail.com>
2021-04-17 23:46:12 +0900

    動的DP

--- AtCoder/src/atCoder300/Beginner129_TypicalStairs.java
+++ AtCoder/src/atCoder300/Beginner129_TypicalStairs.java
@@ -17,44 +17,23 @@ public class Beginner129_TypicalStairs {
 		String strs[] = reader.readLine().split(" ");
 		final int N = Integer.parseInt(strs[0]);
 		final int M = Integer.parseInt(strs[1]);
-		long ans = 1;
 		long mod = (long) (1e9+7);
 		boolean[] step =new  boolean[N+1];
 		for(int i= 0;i<M;i++) {
 			step [Integer.parseInt(reader.readLine())]=true;
 		}
-		int start =0;
-		for(int i=0;i<=N;i++) {
-			if(step[i] || i==N) {
-				//2段連続していたら0
-				if(i-start==0) {
-					return 0;
-				}
-				//対象の段数
-				int stepCount = i==N?i-start:i-start-1;
-				//2段の最大値
-				int twoCount = stepCount/2;
-				long sectionPattern=0;
-				// 全部1段ずつのパターン
-				sectionPattern++;
-				for(int j= 1;j<=twoCount;j++) {
-					sectionPattern +=  modFactorial(stepCount-j,mod)/(modFactorial(j,mod)*modFactorial(stepCount-j-j, mod));
-					sectionPattern %= mod;
-				}
-				ans *= sectionPattern;
-				ans%=mod;
-				start = i+1;
-			}
+		int dp[] = new int[N+1];
+		dp[0]=1;
+		if(!step[1])
+			dp[1]=1;
+		for(int  i= 2;i<=N;i++) {
+			dp[i]+= step[i-1]?0:dp[i-1];
+			dp[i]+= step[i-2]?0:dp[i-2];
+			if(dp[i]==0)
+				return 0;
+			dp[i] %= mod;
 		}
-		return ans;
-	}
-	
-	public static long modFactorial(int n,long mod) {
-		long ans=1;
-		while(n==1) {
-			ans*=n--;
-			ans%=mod;
-		}
-		return ans;
+		
+		return dp[N];
 	}
 }
