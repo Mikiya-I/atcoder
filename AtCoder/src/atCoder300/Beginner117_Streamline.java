1bc998841f43e80181889a291e0e79d41a9ceef3
Mikiya-I <mikiya19961012@gmail.com>
2021-04-21 21:33:40 +0900

    区間問題

--- AtCoder/src/atCoder300/Beginner117_Streamline.java
+++ AtCoder/src/atCoder300/Beginner117_Streamline.java
@@ -3,6 +3,7 @@ package atCoder300;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Beginner117_Streamline {
 
@@ -16,16 +17,26 @@ public class Beginner117_Streamline {
 		String[] strs = reader.readLine().split(" ");
 		final int N = Integer.parseInt(strs[0]);
 		final int M = Integer.parseInt(strs[1]);
+		//NがM以上なら0手で行ける　
 		if(N>=M)
 			return 0;
 		String[] nums = reader.readLine().split(" ");
 		reader.close();
-		boolean[] goal = new boolean[20001 ];
+		int[] points = new int[M];
 		for (int i = 0; i <M; i++) {
-			goal[Integer.parseInt(nums[i])+10000] = true;
+			points[i] = Integer.parseInt(nums[i])+20000;
+		}
+		Arrays.sort(points);
+		int[] distances = new int[M-1];
+		for(int i=0;i<M-1;i++) {
+			distances[i] = points[i+1]-points[i];
+		}
+		Arrays.sort(distances);
+		int ans = points[M-1]-points[0];
+		for(int i=1;i<N;i++) {
+			ans-= distances[M-i-1];
 		}
 		
-		
-		return 1;
+		return ans;
 	}
 }
