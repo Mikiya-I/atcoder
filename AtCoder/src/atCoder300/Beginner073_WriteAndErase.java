81b84773ecab1e0b8bf968f83a6471f58cfa3b8f
Mikiya-I <mikiya19961012@gmail.com>
2021-05-13 12:18:32 +0900

    HashMap→HashSetに書き換え

--- AtCoder/src/atCoder300/Beginner073_WriteAndErase.java
+++ AtCoder/src/atCoder300/Beginner073_WriteAndErase.java
@@ -3,8 +3,7 @@ package atCoder300;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.HashSet;
 
 public class Beginner073_WriteAndErase {
 
@@ -16,22 +15,32 @@ public class Beginner073_WriteAndErase {
 	public static long slove() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		final int N = Integer.parseInt(reader.readLine());
-		HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
-		
+//		HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+//		
+//		for(int i= 0;i<N;i++) {
+//			int tmp = Integer.parseInt(reader.readLine());
+//			if(map.containsKey(tmp)) {
+//				map.replace(tmp, map.get(tmp)+1);
+//			}else {
+//				map.put(tmp, 1);
+//			}
+//		}
+//		
+//		long ans = 0;
+//		for(Entry<Integer, Integer> set : map.entrySet()) {
+//			ans += set.getValue() %2 ;
+//		}
+		HashSet<Integer>  set = new HashSet<Integer>();
 		for(int i= 0;i<N;i++) {
 			int tmp = Integer.parseInt(reader.readLine());
-			if(map.containsKey(tmp)) {
-				map.replace(tmp, map.get(tmp)+1);
+			if(set.contains(tmp)) {
+				set.remove(tmp);
 			}else {
-				map.put(tmp, 1);
+				set.add(tmp);
 			}
 		}
+		return set.size();
 		
-		long ans = 0;
-		for(Entry<Integer, Integer> set : map.entrySet()) {
-			ans += set.getValue() %2 ;
-		}
-		
-		return ans ;
+//		return ans ;
 	}
 }
