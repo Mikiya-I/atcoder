30dff63114825b7699ca5cbb1c6e31259e099b32
Mikiya-I <mikiya19961012@gmail.com>
2021-05-26 12:46:52 +0900

    重複ありの順列の公式&オーバーフロー対策

--- AtCoder/src/atCoder400/Beginner202_D.java
+++ AtCoder/src/atCoder400/Beginner202_D.java
@@ -22,41 +22,13 @@ public class Beginner202_D {
 		int bCnt =0;
 		int aCnt =0;
 		char[] c = new char[(int) (A+B)];
-		//↓二分探索の応用(aとbが同じ数じゃないと無理)
-//		long left = -1;
-//		long right = calTotal(A+B,A,B)+1;
-//		long mid = (left + right) /2;
-//		int i= 0;
-//		while(B > bCnt && A >aCnt) {
-//			if(K > mid) {
-//				c[i] ='b';
-//				bCnt++;
-//				left = mid;
-//			}else {
-//				c[i] ='a';
-//				aCnt++;
-//				right  = mid;
-//			}
-//			mid =  (left + right) /2;
-//			i++;
-//		}
-//		if(B == bCnt) {
-//			while(i<A+B) {
-//				c[i] ='a';
-//				i++;
-//			}
-//		}else {
-//			while(i<A+B) {
-//				c[i] ='b';
-//				i++;
-//			}
-//		}
+
 		int i = 0;
 		while(B > bCnt && A >aCnt) {
 			//通り数
 			long total = calTotal(A-aCnt+B-bCnt,A-aCnt,B-bCnt);
 			//先頭文字がaである通りの数
-			long pattern = total/(A-aCnt+B-bCnt)*(A-aCnt);
+			long pattern = total*(A-aCnt)/(A-aCnt+B-bCnt);
 			if(K > pattern) {
 				c[i] ='b';
 				bCnt++;
@@ -82,20 +54,16 @@ public class Beginner202_D {
 		return new String(c);
 	}
 	
-	//ここの修正
 	public static long calTotal(long x,long y,long z) {
 		long val =1;
-		
-		while ( x >1) {
-			
-			val *= x--;
-			if(val %y ==0 && y>1) {
-				val /=y;
-				y--;
-			}
-			if(val%z == 0 && z>1) {
-				val /= z;
-				z--;
+		long max = Math.max(y, z);
+		long min = Math.min(y, z);
+		long start = max+1;
+		long divide = 2;
+		while ( x >= start) {
+			val *= start++;
+			if(val%divide == 0 && divide<=min) {
+				val /= divide++;
 			}
 		}
 		return val;
