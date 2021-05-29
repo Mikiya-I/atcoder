791f624df122074d92493a0e8a451f9cede3c0b7
Mikiya-I <mikiya19961012@gmail.com>
2021-05-29 20:57:25 +0900

    unionFind

---
+++ AtCoder/src/algorithm/UnionFind.java
@@ -0,0 +1,60 @@
+package algorithm;
+
+class UnionFind {
+	//親要素を保持する配列
+    int[] parents;
+    int[] rank;
+    int[] size;
+    
+    public UnionFind(int n) {
+        this.parents = new int[n+1];
+        this.rank = new int[n+1];
+        this.size = new int[n+1];
+        //初期値は自身が根&サイズ(繋がっている要素の数)とランク(木の高さ)は1
+        for(int i=0;i<=n;i++){
+            parents[i] = i;
+            rank[i] =1;
+            size[i] = 1;
+        }
+    }
+
+    //要素の根を求める
+    public int root(int x){
+       while(parents[x] != x)
+    	   x = parents[x];
+       return x;
+    }
+
+    //xとyの木を結合
+    boolean unite(int x, int y) {
+    	int rootX = root(x);
+    	int rootY = root(y);
+    	//そもそも同じ木だったら何もしない
+    	if(rootX == rootY )
+    		return false;
+    	//結合(ランクが大きい方に小さい方を繋げる)
+    	if(rank[rootX] > rank[rootY]) {
+    		parents[rootY] = rootX;
+    		size [rootX] += size[rootY];
+    	}else {
+    		if (rank[rootX] == rank[rootY])
+    			rank[rootY]++;
+			parents[rootX] = rootY;
+			size[rootY] += size[rootX];
+    	}
+    	return true;
+    }
+
+    //要素x,yが同じグループに属するかどうかの判定
+    boolean  same(int x ,int y) {
+    	int rootX = root(x);
+    	int rootY = root(y);
+    	return rootX == rootY;
+    }
+
+    //サイズの取得
+    int getSize(int x) {
+    	return size[parents[x]];
+    }
+	
+}
