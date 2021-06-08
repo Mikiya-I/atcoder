82f7d15e68ecfffb435492c02fb6e92e373e9904
Mikiya-I <mikiya19961012@gmail.com>
2021-06-09 00:52:21 +0900

    途中

--- AtCoder/src/atCoder300/Beginner204_C.java
+++ AtCoder/src/atCoder300/Beginner204_C.java
@@ -3,10 +3,12 @@ package atCoder300;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
  
 public class Beginner204_C{
 	static long ans =0;
- 
+	static boolean[][] root; 
+	static boolean[] used;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		 
 		slove();
@@ -18,42 +20,66 @@ public class Beginner204_C{
 		String[] strs = reader.readLine().split(" ");
 		final int N = Integer.parseInt(strs[0]);
 		final int M = Integer.parseInt(strs[1]);
-		boolean[][] root = new boolean[N+1][N+1];
+		root = new boolean[N][N];
 		for(int i=0;i<M;i++) {
 			strs = reader.readLine().split(" ");
-			int start = Integer.parseInt(strs[0]);
-			int goal = Integer.parseInt(strs[1]);
+			int start = Integer.parseInt(strs[0])-1;
+			int goal = Integer.parseInt(strs[1])-1;
 			root[start][goal]= true;
 		}
-		for(int i=1;i<=N;i++) {
+		for(int i=0;i<N;i++) {
 			root[i][i]=true;
 		}
+//			for(int i=0;i<N;i++) {
+//				Arrays.fill(used, false);
+//					dfs(i,root[i],used);
+//				}
+//			}
 //		for(int i=1;i<=N;i++) {
 //			boolean[] used = new boolean[N+1];
-//			for(int j=1;j<=N;j++) {
-//				if(root[i][j])
-//			}
+//			Deque<Integer> deque = new ArrayDeque<> ();
+//			bfs(i,root[i],used);
+////			System.out.println();
 //		}
-//		dfs(1,1,root,used);
-//		return 1;
-			for(int i=1;i<=N;i++) {
-				boolean[]used = new boolean[N+1];
-				dfs(i,root,used);
-			}
+		boolean[] used = new boolean[N];
+		for ( int i = 0 ; i < N ; i++ ) {
+			Arrays.fill(used, false);
+			ans += dfs(i);
+		}
 	}
-	
  
 	//深さ優先探索
-		static void dfs(int point,boolean[][] edge,boolean[] used) {
+		static void dfs(int point,boolean[] edge,boolean[] used) {
 			ans++;
+			used[point] = true;
+			//現在の頂点から繋がっている点を探す
+			for(int i= 0; i<edge.length;i++) {
+				//現在の頂点から線が繋がっていて使っていない頂点で再起
+				if(edge[i] &&! used[i]) {
+					dfs(i,root[i],used);
+				}
+			}
+		}
+		
+//		static int dfs(int pos) {
+//			int ret = 1;
+//			used[pos] = true;
+////			for ( int next : root[pos] ) {
+//				if ( !used[next] ) {
+//					ret += dfs(next);
+//				}
+//			}
+//			return ret;
+//		}
+		
+		//幅優先探索
+		static void bfs(int point,boolean[] edge,boolean[] used) {
 			used[point] = true;
 			//現在の頂点から繋がっている点を探す
 			for(int i= 1; i<edge.length;i++) {
 				//現在の頂点から線が繋がっていて使っていない頂点で再起呼び出し、該当する頂点が無ければ再起元に戻る
-				if(edge[point][i] &&! used[i]) {
-					if(!used[i]) {
-						dfs(i,edge,used);
-					}
+				if(edge[i] &&! used[i]) {
+					ans++;
 				}
 			}
 		}
