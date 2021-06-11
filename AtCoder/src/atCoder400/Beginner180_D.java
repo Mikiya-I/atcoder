bb1da61aa64001e76d5f63bb5a5e651f098114ab
Mikiya-I <mikiya19961012@gmail.com>
2021-06-11 22:51:28 +0900

    途中

---
+++ AtCoder/src/atCoder400/Beginner180_D.java
@@ -0,0 +1,38 @@
+package atCoder400;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beginner180_D {
+
+	public static void main(String[] args) throws IOException {
+		
+		long ans = slove();
+		System.out.println(ans);
+	}
+ 
+	public static long slove() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = reader.readLine().split(" ");
+		long x = Long.parseLong(strs[0]);
+		long y = Long.parseLong(strs[1]);
+		long  a = Long .parseLong(strs[2]);
+		long b = Long.parseLong(strs[3]);
+		
+		long difference = y-x;
+		long ans =0;
+		int aCnt = 0;
+		while(difference > x*Math.pow(a, aCnt+1)) {
+			aCnt ++;
+		}
+		
+		for(int i=aCnt; i>0;i--) {
+			long tmp = i;
+			tmp +=( difference-x*Math.pow(i, a))/b;
+			ans = Math.max(ans, tmp);
+		}
+		ans = Math.max(ans, difference/b);
+		return ans;
+	}
+}
