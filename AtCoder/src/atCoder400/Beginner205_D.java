5c27680c2957ca3e76ce8cadf02ab6620bd8c539
Mikiya-I <mikiya19961012@gmail.com>
2021-06-15 22:07:58 +0900

    クエリ先読みでの解法　(途中)

--- AtCoder/src/atCoder400/Beginner205_D.java
+++ AtCoder/src/atCoder400/Beginner205_D.java
@@ -67,25 +67,58 @@ public class Beginner205_D {
 		}
 		Arrays.sort(nums);
 		StringBuilder sb = new StringBuilder();
-		long[] find = new long[Q];
-		for(int i=0;i<Q;i++ ) {
-			find[i] = Long.parseLong(reader.readLine());
-		}
-		//何番目の項目が何番目にスタートするか
-		long[] notFoundStart = new long[N];
-		for(int i= 0,j=0;i<N;i++) {
-			if(j != nums[i]) {
-				
-			}
-			nums[i] = j;
-		}
-		for(int i= 0;i<Q;i++) {
-			//開始位置をどう決めるか?
-			sb.append(findVal(find[i],0)+"\n");
-		}
+		//numsのi番目の要素までに何個の数が見つかっているか(累積和)
+		long[][] interval = new long[N + 1][2];
+        interval[0][0] = 0;
+        interval[0][1] = nums[0] - 1;
+        for (int i = 0; i < N; i++) {
+            interval[i + 1][0] = nums[i] - interval[i][1];
+            interval[i + 1][1] = nums[i + 1] - nums[i] - 1 + interval[i][1];
+        }
+        //Kを読み込み&ソート
+        long[] K = new long[Q];
+        //Kの順番を保持
+        long[] kOrder = new long[Q];
+        for(int i=0;i<Q;i++) {
+        	K[i] = Long.parseLong(strs[i]);
+        	kOrder[i] = K[i];
+        }
+        Arrays.sort(K);
+        
+        
 		return sb.toString();
 	}
 	
+//	public static String  slove() throws IOException {
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//		final String[] strs = reader.readLine().split(" ");
+//		final int N =Integer.parseInt(strs[0]);
+//		final int Q = Integer.parseInt(strs[1]);
+//		String[] strs2 = reader.readLine().split(" ");
+//		nums = new long[N];
+//		for(int i=0;i<N;i++) {
+//			nums[i] = Long.parseLong(strs2[i]);
+//		}
+//		Arrays.sort(nums);
+//		StringBuilder sb = new StringBuilder();
+//		long[] find = new long[Q];
+//		for(int i=0;i<Q;i++ ) {
+//			find[i] = Long.parseLong(reader.readLine());
+//		}
+//		//何番目の項目が何番目にスタートするか
+//		long[] notFoundStart = new long[N];
+//		for(int i= 0,j=0;i<N;i++) {
+//			if(j != nums[i]) {
+//				
+//			}
+//			nums[i] = j;
+//		}
+//		for(int i= 0;i<Q;i++) {
+//			//開始位置をどう決めるか?
+//			sb.append(findVal(find[i],0)+"\n");
+//		}
+//		return sb.toString();
+//	}
 	public static long findVal(long val,int index) {
 		for(int i=index;i<nums.length;i++) {
 			if(nums[i] <= val) {
