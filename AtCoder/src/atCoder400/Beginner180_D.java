6fdcd904ab9dc85961e45d5615a0e640d5a5d11c
Mikiya-I <mikiya19961012@gmail.com>
2021-06-16 23:22:16 +0900

    オーバーフロー対策

--- AtCoder/src/atCoder400/Beginner180_D.java
+++ AtCoder/src/atCoder400/Beginner180_D.java
@@ -19,23 +19,48 @@ public class Beginner180_D {
 		long y = Long.parseLong(strs[1]);
 		long  a = Long .parseLong(strs[2]);
 		long b = Long.parseLong(strs[3]);
-		
-		long difference = y-x;
-		long ans =0;
-		int aCnt = 0;
-		long tmpY = y;
-		while(tmpY >= a) {
-			tmpY /= a;
-			aCnt++;
+		long ans = (y-x)/b;
+		if((x-y)%b == 0) {
+			ans --;
 		}
-		
-		for(int i=aCnt; i>=0;i--) {
-			long tmp = i;
-			long tmp2 =  (long) (y-x*Math.pow(a, i))-1;
-			tmp += tmp2 /b;
+		int i=0;
+		//オーバーフロー対策 && yを超えないように
+		while((double)a*x<=2e18 && a*x<y) {
+			x*=a;
+			i++;
+			long tmp = i+(y-x)/b;
+			//bで割り切れた場合は-1
+			if((y-x)%b ==0) {
+				tmp--;
+			}
 			ans = Math.max(ans, tmp);
 		}
-		ans = Math.max(ans, difference/b);
+//		long difference = y-x-1;
+//		long ans =0;
+//		int aCnt = 0;
+//		long tmpY = y;
+//		tmpY/=a;
+//		while(tmpY > x) {
+//			aCnt++;
+//			tmpY /= a;
+//		}
+//		
+//		for(int i=aCnt; i>=0;i--) {
+//			long tmp = i;
+//			long tmp2 =  (long) (y-x*Math.pow(a, i));
+//			tmp += tmp2 /b;
+//			if(tmp2%b==0)
+//				tmp --;
+//			ans = Math.max(ans, tmp);
+//		}
+//		ans = Math.max(ans, difference/b);
+//		return ans;
+		
+//		while((double)a*x<=2e18 && a*x<=x+b && a*x<y){
+//			x*=a;
+//			ans++;
+//		}
+//		ans+=(y-1-x)/b;
 		return ans;
 	}
 }
