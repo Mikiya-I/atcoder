2c146de9d5021f73506aab357ec891c9fa675905
Mikiya-I <mikiya19961012@gmail.com>
2021-06-24 22:51:22 +0900

    差分更新　(StringBuilderよりBufferedWriterを使った方が速い)

--- AtCoder/src/atCoder400/Beginner171_D.java
+++ AtCoder/src/atCoder400/Beginner171_D.java
@@ -1,21 +1,19 @@
 package atCoder400;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Beginner171_D {
 	public static void main(String[] args) throws IOException {
-		slove();
-//		long  ans = slove();
-//		System.out.println(ans);
+//		slove();
+		String  ans = slove();
+		System.out.println(ans);
 	}
 	
-	public static void slove() throws IOException {
+	public static String slove() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		final int N =Integer.parseInt(reader.readLine());
 		String[] strs = reader.readLine().split(" ");
 		long[] arr = new long[100001];
@@ -35,9 +33,9 @@ public class Beginner171_D {
 			sum+=c*(arr[(int)b]);
 			arr[(int)c] += arr[(int) b];
 			arr[(int)b] =0;
-			writer.write(((Long)sum).toString());
-			writer.newLine();
+			sb.append(sum);
+			sb.append("\n");
 		}
-		writer.flush();
+		return sb.toString();
 	}
 }
