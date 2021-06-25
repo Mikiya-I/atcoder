275518315528cdfcbc31696a3dfe2f34dd24f4ce
Mikiya-I <mikiya19961012@gmail.com>
2021-06-25 23:48:15 +0900

    素数

---
+++ AtCoder/src/atCoder400/Beginner169_D.java
@@ -0,0 +1,22 @@
+package atCoder400;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beginner169_D {
+	public static void main(String[] args) throws IOException {
+//		slove();
+		long  ans = slove();
+		System.out.println(ans);
+	}
+	
+	public static long slove() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		final Long N =Long.parseLong(reader.readLine());
+		int ans =0;
+		
+		return 1;
+	}
+}
