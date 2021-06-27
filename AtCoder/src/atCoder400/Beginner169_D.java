69ab9e3d4057727bcaf52c125c49ac7294142fb3
Mikiya-I <mikiya19961012@gmail.com>
2021-06-27 23:52:08 +0900

    素因数分解

--- AtCoder/src/atCoder400/Beginner169_D.java
+++ AtCoder/src/atCoder400/Beginner169_D.java
@@ -9,14 +9,57 @@ public class Beginner169_D {
 //		slove();
 		long  ans = slove();
 		System.out.println(ans);
+//		System.out.println(isPrime());
 	}
 	
 	public static long slove() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		final Long N =Long.parseLong(reader.readLine());
-		int ans =0;
+		long N =Long.parseLong(reader.readLine());
+		if(N==1) {
+			return 0;
+		}
+		long ans =0;
 		
-		return 1;
+		long root = (long) Math.sqrt(N);
+		for(int i=2;i<=root;i++) {
+			if(N%i==0) {
+				if(isPrime(i)) {
+					long div = i;
+					long tmp = N;
+					while(tmp % div==0) {
+						ans++;
+						tmp /=div;
+						div*=i;
+					}
+				}
+				if(isPrime(N/i)){
+					long div = N/i;
+					long tmp = N;
+					while(tmp % div==0) {
+						ans++;
+						tmp /=div;
+						div*=N/i;
+					}
+				}
+			}
+		}
+		if(isPrime(N)) {
+			 ans++;
+		}
+		return ans;
+	}
+	
+	public static boolean isPrime(long x) {
+		if(x !=2 && x%2==0) {
+			return false;
+		}else if(x==2) {
+			return true;
+		}
+		for(int i=3;i<=Math.sqrt(x);i+=2) {
+			if(x%i==0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
