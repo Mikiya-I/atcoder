770457440731b93efd4f7e72d92d1567ea4fc056
Mikiya-I <mikiya19961012@gmail.com>
2021-07-20 23:46:22 +0900

    三角関数

--- AtCoder/src/atCoder400/Beginner144_D.java
+++ AtCoder/src/atCoder400/Beginner144_D.java
@@ -17,7 +17,6 @@ public class Beginner144_D {
 		double a = Integer.parseInt(strs[0]);
 		double b = Integer.parseInt(strs[1]);
 		double x = Integer.parseInt(strs[2]);
-		double ans =0;
 		
 		boolean overHalf;
 		//水が半分以上を占めていたら空間が、そうでなければ水があふれる直前に三角形になる
@@ -29,15 +28,17 @@ public class Beginner144_D {
 		//水か空間の小さい方の面積を求める
 		double m;
 		if(overHalf)
-			m=a*b/2 - x/a;
+			m=a*b - x/a;
 		else 
 			m=x/a;
 		
 		//あふれる直前の三角形の底辺(高さはbであると確定)
-		double btm = m/b;
+		double btm = m*2/b;
 		//三平方の定理で斜辺を計算
 		double hypotenuse = Math.sqrt(Math.pow(b, 2)+Math.pow(btm, 2));
 		//斜辺と底辺間の角度が答えになる
+		double ans = Math.asin(b/hypotenuse);
+		ans= Math.toDegrees(ans);
 		return ans;
 	}
 }
