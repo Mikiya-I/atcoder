62a4746016ff89ea2ff37c86d3ffb377eaa5f3ee
Mikiya-I <mikiya19961012@gmail.com>
2021-07-26 22:33:52 +0900

    DP

--- AtCoder/src/atCoder300/Beginner211_C.java
+++ AtCoder/src/atCoder300/Beginner211_C.java
@@ -3,6 +3,7 @@ package atCoder300;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
  
 public class Beginner211_C{
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -16,7 +17,20 @@ public class Beginner211_C{
 		long mod = (long) (1e9+7);
 		char[] str = reader.readLine().toCharArray();
 		reader.close();
-		int[][] dp = new int[str.length][8];
+		final char[]	 S= "chokudai".toCharArray();
+		int[][] dp = new int[8+1][str.length+1];
 		
+		Arrays.fill(dp[0], 1);
+		for(int i=1;i<=8;i++) {
+			for(int j=1;j<=str.length;j++) {
+				if(str[j-1]==S[i-1]) {
+					int x= (int)(((long) dp[i-1][j]+(long)dp[i][j-1])%mod);
+					dp[i][j]= x;
+				}else {
+					dp[i][j]=dp[i][j-1];
+				}
+			}
+		}
+		return dp[8][str.length];
 	}
 }
