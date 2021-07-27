f87243e1bff68f7438da75d353f0cc9f2009f54b
Mikiya-I <mikiya19961012@gmail.com>
2021-07-27 23:39:12 +0900

    BFS

--- AtCoder/src/atCoder400/Beginner211_D.java
+++ AtCoder/src/atCoder400/Beginner211_D.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class Beginner211_D {
-	static ArrayList<ArrayList<Integer>> path;
 	public static void main(String[] args) throws IOException {
 //		long  ans = slove();
 //		slove();
@@ -18,11 +17,13 @@ public class Beginner211_D {
 	public static long slove() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = reader.readLine().split(" ");
-		long mod = (long) 1e+9;
+		int mod = (int) 1e+9;
 		final int N = Integer.parseInt(strs[0]);
 		final int M = Integer.parseInt(strs[1]);
-		path = new ArrayList<ArrayList<Integer>>();
+//		List<HashSet<Integer>> path = new ArrayList<HashSet<Integer>>();
+		List<List<Integer>> path = new ArrayList<List<Integer>>();
 		for(int i=0;i<N+1;i++) {
+//			path.add(new HashSet<Integer>());
 			path.add(new ArrayList<Integer>());
 		}
 		//道をlistに入れる
@@ -31,33 +32,31 @@ public class Beginner211_D {
 			path.get(Integer.parseInt(strs[0])).add(Integer.parseInt(strs[1]));
 			path.get(Integer.parseInt(strs[1])).add(Integer.parseInt(strs[0]));
 		}
+		//↑　どこかでバグってる
 		ArrayDeque<Integer> que = new ArrayDeque<Integer>();
 		que.add(1);
 		//そこまでに行くのに何回移動したか
-		int[] disFrom1 = new int[N+1];
-		Arrays.fill(disFrom1, -1);
-		disFrom1[1] = 0;
+		int[] dis = new int[N+1];
 		//そこに行けるルートの数
 		int[] rootCnt = new int[N+1];
-		rootCnt[1]++;
-	    while(que.size()>0) {
+		rootCnt[1]=1;
+		
+	    while(!que.isEmpty()) {
 	    	int tmp = que.pop();
 	    	for(int i:path.get(tmp)) {
 	    		//初めて見つかるルート
-	    		if(disFrom1[i]==-1) {
-	    			disFrom1[i]= disFrom1[tmp]+1;
+	    		if(dis[i]==0) {
+	    			dis[i]= dis[tmp]+1;
 	    			rootCnt[i] = rootCnt[tmp];
 	    			que.add(i);
 	    		//元々見つかっていたルート
 	    		}else {
-	    			//同じ道が複数回あるとダブる
-	    			if(disFrom1[i]==disFrom1[tmp]+1) {
-	    				rootCnt[i] = (int) ((long)(rootCnt[i] + (long)rootCnt[tmp])%mod);
+	    			if(dis[i]==dis[tmp]+1) {
+	    				rootCnt[i]=(rootCnt[i]+rootCnt[tmp])%mod;
 	    			}
 	    		}
 	    	}
 	    }
-	    
 		return rootCnt[N];
 	}
 }
