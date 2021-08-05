9386e9a4b993c1bce58ed2492dbbb48f06aa7070
Mikiya-I <mikiya19961012@gmail.com>
2021-08-05 23:31:11 +0900

    区間スケジューリング問題

---
+++ AtCoder/src/atcoder90/q014.java
@@ -0,0 +1,39 @@
+package atcoder90;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class q014 {
+	public static void main(String[] args) throws IOException {
+//		slove();
+		System.out.println(slove());
+	}
+	
+	public static long slove() throws  IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		final int N = Integer.parseInt(reader.readLine());
+		int[] students = new int[N];
+		String[] strs = reader.readLine().split(" ");
+		for(int i=0;i<N;i++) {
+			students[i] = Integer.parseInt(strs[i]);
+		}
+		
+		strs=reader.readLine().split(" ");
+		reader.close();
+		int schools[] = new int [N];
+		for(int i=0;i<N;i++) {
+			schools[i] = Integer.parseInt(strs[i]);
+		}
+		Arrays.sort(students);
+		Arrays.sort(schools);
+		long ans =0;
+		for(int i=0;i<N;i++) {
+			ans += (long)Math.abs(students[i] - schools[i]);
+		}
+		
+		
+		return ans;
+	}
+}
