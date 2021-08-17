085760352bcc7e9b6a51c278213223c5592f7ebc
Mikiya-I <mikiya19961012@gmail.com>
2021-08-17 23:56:01 +0900

    二分探索木

--- AtCoder/src/algorithm/BinarySearchSample.java
+++ AtCoder/src/algorithm/BinarySearchSample.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class BinarySearchSample {
 	
 	int binarySearch(List<Integer> list, int x) {
+		//left x未満　right x以上
 		int left = -1;
 		int right = list.size();
 		while(right-left >1) {
