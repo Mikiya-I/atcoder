0bb9052622e96de003b1de7184e26ab74c93f5dc
Mikiya-I <mikiya19961012@gmail.com>
2021-08-27 00:31:12 +0900

    gcd オーバーフロー対策(BigInteger未使用)

--- AtCoder/src/atcoder90/q038.java
+++ AtCoder/src/atcoder90/q038.java
@@ -3,7 +3,6 @@ package atcoder90;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class q038 {
 	public static void main(String[] args) throws IOException {
@@ -19,20 +18,20 @@ public class q038 {
 		
 		long e18 = (long)1e18;
 		long cd = gcd(a,b);
-		BigInteger A = new BigInteger(strs[0]);
-		BigInteger B = new BigInteger(strs[1]);
-		BigInteger C = BigInteger.valueOf(cd);
-		BigInteger E18 = BigInteger.valueOf(e18);
-//		if(a/cd > e18/ b)
-//			System.out.println("Large");
-//		else
-//			System.out.println(Math.max(a, b));
-		BigInteger AB = A.multiply(B);
-		BigInteger ABC = AB.divide(C);
-		if(ABC.compareTo(E18) > 0)
+//		BigInteger A = new BigInteger(strs[0]);
+//		BigInteger B = new BigInteger(strs[1]);
+//		BigInteger C = BigInteger.valueOf(cd);
+//		BigInteger E18 = BigInteger.valueOf(e18);
+		if(a/cd > e18/ b)
 			System.out.println("Large");
 		else
-			System.out.println(ABC);
+			System.out.println(a/cd* b);
+//		BigInteger AB = A.multiply(B);
+//		BigInteger ABC = AB.divide(C);
+//		if(ABC.compareTo(E18) > 0)
+//			System.out.println("Large");
+//		else
+//			System.out.println(ABC);
 	}
 	
 	private static long gcd(long a,long b) {
