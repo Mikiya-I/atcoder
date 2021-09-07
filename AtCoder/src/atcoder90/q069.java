bceb4757d30424954a8838dacd0dbd17806b1687
Mikiya-I <mikiya19961012@gmail.com>
2021-09-07 21:08:23 +0900

    繰り返し二乗法

--- AtCoder/src/atcoder90/q069.java
+++ AtCoder/src/atcoder90/q069.java
@@ -14,28 +14,35 @@ public class q069 {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = reader.readLine().split(" ");
 		reader.close();
-		long N = Integer.parseInt(strs[0]);
-		final int K = Integer.parseInt(strs[1]);
-		int e97 = (int)1e9+7;
+		long N = Long.parseLong(strs[0]);
+		final long K = Long.parseLong(strs[1]);
+		if(N==1) {
+			return K;
+		}
+		long e97 = (long)1e9+7;
 		long ans = (K*(K-1))%e97;
 		long n = N-2;
-		
+		ans *= modPow(n, K-2, e97);
+		ans %= e97;
 		return ans;
 	}
 	
-	private static long modPow(long x, long n,long Mod) {
-		if(n < 1) {
-			return 0;
-		}
-		long k = 1;
-		while( n > 1 ) {
-			if( n%2 == 1 ) k *= x;
-			x *= x;
-			n /= 2;
-			x %= Mod;
-			k %= Mod;
+	//x:基数　
+	private static long modPow(long n, long x,long mod) {
+		long ans = 1;
+		//基数の乗数
+		long tmp=x;
+		//nをビット列として扱う
+		while(n>0) {
+			if((n & 1)==1)  {
+				ans *= tmp;
+				ans %= mod;
+			}
+			tmp *= tmp;
+			tmp %= mod;
+			n = n>>1;
 		}
 		
-		return (k*x)%Mod;
+		return ans;
 	}
 }
