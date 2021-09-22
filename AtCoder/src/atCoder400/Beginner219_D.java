c036da618c330fc0f345d2187ecf162d4d0b7ef7
Mikiya-I <mikiya19961012@gmail.com>
2021-09-22 22:43:59 +0900

    ナップザック問題(最小値)

--- AtCoder/src/atCoder400/Beginner219_D.java
+++ AtCoder/src/atCoder400/Beginner219_D.java
@@ -10,7 +10,7 @@ public class Beginner219_D {
 	public static void main(String[] args) throws IOException {
 		//		long  ans = slove();
 		//		slove();
-		System.out.println(slove());
+		System.out.println(slove2());
 	}
 
 	public static long slove() throws IOException {
@@ -20,11 +20,11 @@ public class Beginner219_D {
 		final int X = Integer.parseInt(strs[0]);
 		final int Y = Integer.parseInt(strs[1]);
 		//n番目までの要素でX,Yの個数以上になるための最小値
-		int[][][] dp = new int[N+1][X+1][Y+1];
-		Arrays.fill(dp,Integer.MAX_VALUE );
+		int[][][] dp = new int[N + 1][X + 1][Y + 1];
+		Arrays.fill(dp, Integer.MAX_VALUE);
 		dp[0][0][0] = 0;
-		int aSum = 0,bSum = 0;
-		for(int i=1;i<=N;i++) {
+		int aSum = 0, bSum = 0;
+		for (int i = 1; i <= N; i++) {
 			strs = reader.readLine().split(" ");
 			int a = Integer.parseInt(strs[0]);
 			aSum += a;
@@ -32,43 +32,48 @@ public class Beginner219_D {
 			bSum += b;
 			dp[i][a][b] = 1;
 		}
-		if(aSum < X ||bSum < Y) {
+		if (aSum < X || bSum < Y) {
 			return -1;
 		}
 		//dp計算
-		for(int i =1;i<N;i++) {
-			for(int j=0;j<X;j++) {
-				for(int k=0;k<Y;k++) {
-					
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < X; j++) {
+				for (int k = 0; k < Y; k++) {
+
 				}
 			}
 		}
-		
+
 		return dp[N][X][Y];
 	}
+
 	private static long slove2() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		final int n = Integer.parseInt(reader.readLine());
 		String[] strs = reader.readLine().split(" ");
 		final int x = Integer.parseInt(strs[0]);
 		final int y = Integer.parseInt(strs[1]);
-        int[][] dp = new int[x + 1][y + 1];
-        int INF = Integer.MAX_VALUE;
-        for (int[] is : dp) 
-        	Arrays.fill(is, INF);
- 
-        dp[0][0] = 0;
-        for (int i = 0; i < n; i++) {
-        	strs = reader.readLine().split(" ");
+		int[][] dp = new int[x + 1][y + 1];
+		int INF = n+1;
+		for (int[] is : dp)
+			Arrays.fill(is, INF);
+
+		dp[0][0] = 0;
+		//n個の要素
+		for (int i = 0; i < n; i++) {
+			strs = reader.readLine().split(" ");
 			int a = Integer.parseInt(strs[0]);
 			int b = Integer.parseInt(strs[1]);
-            for (int j = x; j >= 0; j--) { for (int k = y; k >= 0; k--) {
-                int j2 = Math.min(j + a, x);
-                int k2 = Math.min(k + b, y);
-                dp[j2][k2] = Math.min(dp[j2][k2], 1 + dp[j][k]);
-                }
-            }
-        }
-        return dp[x][y]==INF?-1:dp[x][y];
+			//xから0までの最小値を更新
+			for (int j = x; j >= 0; j--) {
+				//yから0までの最小値を更新
+				for (int k = y; k >= 0; k--) {
+					int j2 = Math.min(j + a, x);
+					int k2 = Math.min(k + b, y);
+					dp[j2][k2] = Math.min(dp[j2][k2], 1 + dp[j][k]);
+				}
+			}
+		}
+		return dp[x][y] == INF ? -1 : dp[x][y];
 	}
 }
