4ab83b62f763166de49a8140c346520fdeed7ef2
Mikiya-I <mikiya19961012@gmail.com>
2021-09-24 23:41:13 +0900

    dfs (tle)

---
+++ AtCoder/src/atcoder90/q003.java
@@ -0,0 +1,67 @@
+package atcoder90;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class q003 {
+	static List<Integer>[] path;
+	static int N;
+	static boolean[] used ;
+	static ArrayDeque<Integer> que;
+	static int tmp;
+	static int maxDepth;
+	public static void main(String[] args) throws IOException {
+//		slove();
+		System.out.println(slove());
+	}
+	
+	public static long slove() throws  IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(reader.readLine());
+		String[] strs ;
+		used = new boolean[N];
+		path = new ArrayList[N];
+		for(int i=0;i<N;i++) {
+			path[i] = new ArrayList<Integer>();
+		}
+		for(int i=0;i<N-1 ;i++) {
+			strs = reader.readLine().split(" ");
+			int a = Integer.parseInt(strs[0])-1;
+			int b = Integer.parseInt(strs[1])-1;
+			path[a].add(b);
+			path[b].add(a);
+		}
+		que = new ArrayDeque<Integer>();
+		for(int i= 0;i<N;i++) {
+			Arrays.fill(used, false);
+			tmp = 0;
+			que.add(i);
+			dfs();
+		}
+		
+		return maxDepth+1;
+	}
+	
+	private static void dfs() {
+		while(que.size() > 0) {
+			tmp++;
+			int x = que.pop();
+			used[x] = true;
+			for(int point : path[x]) {
+				if(!used[point]) {
+					que.add(point);
+					dfs();
+					maxDepth=Math.max(maxDepth, tmp);
+				}
+			}
+			tmp--;
+		}
+		
+		
+	}
+}
