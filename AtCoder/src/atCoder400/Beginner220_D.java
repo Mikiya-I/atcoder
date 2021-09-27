068009ab334c4b0333c41ef28edff52705bc2165
Mikiya-I <mikiya19961012@gmail.com>
2021-09-27 18:06:05 +0900

    dp

--- AtCoder/src/atCoder400/Beginner220_D.java
+++ AtCoder/src/atCoder400/Beginner220_D.java
@@ -3,7 +3,6 @@ package atCoder400;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Beginner220_D {
 
@@ -19,29 +18,28 @@ public class Beginner220_D {
 		final int N = Integer.parseInt(reader.readLine());
 		String[] strs = reader.readLine().split(" ");
 		reader.close();
-		int[] beforeCnt = new int[10];
-		int[] tmpCnt = new int[10];
+//		int[] beforeCnt = new int[10];
+//		int[] tmpCnt = new int[10];
+		int[][] dp = new int[N][10];
 		int x= Integer.parseInt(strs[0]);
 		int y= Integer.parseInt(strs[1]);
-		tmpCnt[ (x+y)%10] ++;
-		tmpCnt[ (x*y)%10] ++;
+		dp[1][ (x+y)%10] ++;
+		dp[1][ (x*y)%10] ++;
 		for(int i=2;i<N;i++) {
 			y= Integer.parseInt(strs[i]);
-			beforeCnt = tmpCnt.clone();
-			Arrays.fill(tmpCnt, 0);
 			for(int j=0;j<10;j++) {
-				if(beforeCnt[j]==0)
+				if(dp[i-1][j]==0)
 					continue;
 				int f =  (j+y)%10;
 				int g = (j*y)%10;
-				tmpCnt[f] += beforeCnt[j] ;
-				tmpCnt[f] %= mod;
-				tmpCnt[g] += beforeCnt[j];
-				tmpCnt[g] %= mod;
+				dp[i][f] += dp[i-1][j] ;
+				dp[i][f]  %= mod;
+				dp[i][g] += dp[i-1][j];
+				dp[i][g] %= mod;
 			}
 		}
 		
-		for(int ans: tmpCnt)
+		for(int ans: dp[N-1])
 			System.out.println(ans);
 	}
 }
