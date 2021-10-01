253d6b3167df60147f3c7b3354107638fc7240ae
Mikiya-I <mikiya19961012@gmail.com>
2021-10-01 22:17:27 +0900

    dfs&2部グラフ(途中)

---
+++ AtCoder/src/atcoder90/q026.java
@@ -0,0 +1,65 @@
+package atcoder90;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Set;
+
+public class q026 {
+	
+	static Set<Integer>[] path;
+	static ArrayDeque<Integer> que;
+	static boolean[] used ;
+	//2部グラフ
+	static boolean[] pattern;
+	static int trueCnt ;
+	
+	public static void main(String[] args) throws IOException {
+//		slove();
+		System.out.println(slove());
+	}
+	
+	public static String slove() throws  IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		final int N = Integer.parseInt(reader.readLine());
+		path = new HashSet[N];
+		used = new boolean[N];
+		for(int i=0;i<N;i++) {
+			path[i] = new HashSet<Integer>();
+		}
+		
+		for(int i=0;i<N-1;i++) {
+			String[] strs = reader.readLine().split(" ");
+			int A = Integer.parseInt(strs[0])-1;
+			int B = Integer.parseInt(strs[1])-1;
+			path[A].add(B);
+			path[B].add(A);
+		}
+		
+		que = new ArrayDeque<Integer>();
+		que.add(0);
+		trueCnt =0;
+		dfs();
+		
+		return sb.toString();
+	}
+	
+	private static void dfs() {
+		while(que.size()>0) {
+			int tmp = que.pop();
+			for(int i:path[tmp]) {
+				if(!used[i]) {
+					used[i] = true;
+					pattern[i] =  ! pattern[tmp];
+					que.add(i);
+					dfs();
+				}
+			}
+		}
+	}
+	
+}
