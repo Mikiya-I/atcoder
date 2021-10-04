8f1048d94d5f1fb98dc78de4b45d7fd945648091
Mikiya-I <mikiya19961012@gmail.com>
2021-10-05 01:38:46 +0900

    二次元いもす法

--- AtCoder/src/atcoder90/q028.java
+++ AtCoder/src/atcoder90/q028.java
@@ -3,7 +3,6 @@ package atcoder90;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.TreeMap;
 
 public class q028 {
 	
@@ -17,22 +16,43 @@ public class q028 {
 		StringBuilder sb = new StringBuilder();
 		
 		final int N = Integer.parseInt(reader.readLine());
-		TreeMap<Integer,TreeMap<Integer,Integer>>  event = new TreeMap<Integer, TreeMap<Integer, Integer>>();
-		
+		int[][] grid = new int[1001][1001];
 		for(int i=0;i<N;i++) {
 			String[] strs = reader.readLine().split(" ");
-			int x1 = Integer.parseInt(strs[i]);
-			int y1 = Integer.parseInt(strs[i]);
-			int x2 = Integer.parseInt(strs[i]);
-			int y2 = Integer.parseInt(strs[i]);
-			if(!event.containsKey(y1)) {
-				event.put(y1,new TreeMap<Integer,Integer>());
+			int x1 = Integer.parseInt(strs[0]);
+			int y1 = Integer.parseInt(strs[1]);
+			int x2 = Integer.parseInt(strs[2]);
+			int y2 = Integer.parseInt(strs[3]);
+			grid[y1][x1] ++;
+			grid[y1][x2]--;
+			grid[y2][x1]--;
+			grid[y2][x2]++;
+		}
+		
+		//累積和計算
+		//x軸方向
+		for(int i=0;i<grid.length;i++) {
+			for(int j=1;j<grid[i].length;j++) {
+				grid[i][j] += grid[i][j-1];
 			}
-			if(!event.get(y1).containsKey(x1)) {
-				event.get(y1).put(x1, 1);
-			}else {
-//				event.get(y1).put(x1, event.get(strs));
+		}
+		//y軸方向
+		for(int i=1;i<grid.length;i++) {
+			for(int j=0;j<grid[i].length;j++) {
+				grid[i][j] += grid[i-1][j];
 			}
+		}
+		
+		int[] sums = new int[N+1];
+		for(int[] arr:grid) {
+			for(int i:arr) {
+				sums[i]++;
+			}
+		}
+		
+		for(int i = 1;i<N+1;i++) {
+			sb.append(sums[i]);
+			sb.append("\n");
 		}
 		
 		return sb.toString();
