bf1f96c9d81cfc7a3262e34cecdc3d3369006098
Mikiya-I <mikiya19961012@gmail.com>
2021-10-15 21:46:37 +0900

    ダイクストラ コスト追加

--- AtCoder/src/algorithm/Dijkstra.java
+++ AtCoder/src/algorithm/Dijkstra.java
@@ -8,6 +8,10 @@ import java.util.Set;
 public class Dijkstra {
 	//地点間の距離
 	int distance[][];
+	//地点でのコスト
+	int[] cost;
+	//スタートからその地点までの最小コスト
+	int[] minCost;
 	//地点数
 	int point;
 	int startPoint;
@@ -18,64 +22,112 @@ public class Dijkstra {
 	int[] minDistances;
 	//その地点への最短経路が確定されているか
 	boolean[] fixed;
-	//パス
-	Map<Integer,Set<Integer>> path;
-	
-	public Dijkstra(int[][] dist,int start,int goal) {
+	//エッジ
+	Map<Integer, Set<Integer>> path;
+
+	public Dijkstra(int[][] dist, int start, int goal,int nodesCnt) {
+		minDistances = new int[nodesCnt];
+		minCost = new int[nodesCnt];
+		fixed = new boolean[nodesCnt];
 		Arrays.fill(minDistances, -1);
+		Arrays.fill(minCost, -1);
 		distance = dist;
 		startPoint = start;
 		goalPoint = goal;
 	}
-		
+
+	//コスト計算時に使用
+	public void setCost(int[][] costGrid) {
+		int rows = costGrid.length;
+		int columuns = costGrid[0].length;
+		cost = new int[costGrid.length * costGrid[0].length];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columuns; j++) {
+				//二次元配列を一次元配列に変換し、コストを代入
+				cost[i * costGrid[0].length + j] = costGrid[i][j];
+			}
+		}
+	}
+
+	//ゴールまでの最小コスト
+	public void calcMinCost() {
+		PriorityQueue<Node> nodes = new PriorityQueue<Node>();
+		//キューにスタート地点を追加する
+		nodes.add(new Node(startPoint, 0));
+
+		while (!nodes.isEmpty()) {
+			//キュー内の最小値の地点
+			Node current = nodes.poll();
+			int tmpStartPoint = current.point;
+			//既に確定済みのものに関してはスキップ
+			if (fixed[tmpStartPoint])
+				continue;
+			fixed[startPoint] = true;
+			minCost[startPoint]=current.distanceFromStart;
+			//目的地が確定済みなら終わり
+			if (fixed[goalPoint])
+				break;
+			//キューにスタート地点から繋がっている地点を入れる。入っているものに関しては更新
+			for (int adjacent : path.get(tmpStartPoint)) {
+				int tmpCost = minCost[tmpStartPoint] + cost[adjacent];
+				//未確定の地点の中で最短経路を更新したものか初めて見つかった地点
+				if (!fixed[adjacent] && (minCost[adjacent] > tmpCost || minCost[adjacent] == -1)) {
+					minCost[adjacent] = tmpCost;
+					nodes.add(new Node(adjacent, tmpCost));
+				}
+			}
+
+		}
+	}
+
 	//ゴール地点まで
 	public void toGoal() {
 		PriorityQueue<Node> nodes = new PriorityQueue<Node>();
 		//キューにスタート地点を追加する
 		nodes.add(new Node(startPoint, 0));
-		
-		while(!nodes.isEmpty()) {
+
+		while (!nodes.isEmpty()) {
 			//キュー内の最小値の地点
 			Node current = nodes.poll();
 			int tmpStartPoint = current.point;
 			//既に確定済みのものに関してはスキップ
-			if(fixed[tmpStartPoint])
+			if (fixed[tmpStartPoint])
 				continue;
-			fixed[startPoint]=true;
+			fixed[startPoint] = true;
 			//目的地が確定済みなら終わり
-			if(fixed[goalPoint])
+			if (fixed[goalPoint])
 				break;
 			//キューにスタート地点から繋がっている地点を入れる。入っているものに関しては更新
-			for(int adjacent:path.get(startPoint)) {
-				int tmpDist = distance[startPoint][tmpStartPoint]+distance[tmpStartPoint][adjacent];
+			for (int adjacent : path.get(startPoint)) {
+				int tmpDist = distance[startPoint][tmpStartPoint] + distance[tmpStartPoint][adjacent];
 				//未確定の地点の中で最短経路を更新したものか初めて見つかった地点
-				if(!fixed[adjacent] && (minDistances[adjacent]>tmpDist || minDistances[adjacent] == -1)) {
+				if (!fixed[adjacent] && (minDistances[adjacent] > tmpDist || minDistances[adjacent] == -1)) {
 					minDistances[adjacent] = tmpDist;
 					nodes.add(new Node(adjacent, tmpDist));
 				}
 			}
-			
+
 		}
 	}
-	
+
 	//行ける所まで
 	public void calcAll() {
 		PriorityQueue<Node> nodes = new PriorityQueue<Node>();
 		//キューにスタート地点を追加する
 		nodes.add(new Node(startPoint, 0));
-		
-		while(!nodes.isEmpty()) {
+
+		while (!nodes.isEmpty()) {
 			//キュー内の最小値の地点
 			Node current = nodes.poll();
 			int tmpStartPoint = current.point;
 			//既に確定済みのものに関してはスキップ
-			if(fixed[tmpStartPoint])
+			if (fixed[tmpStartPoint])
 				continue;
-			fixed[startPoint]=true;
+			fixed[startPoint] = true;
 			//キューにスタート地点から繋がっている地点を入れる。入っているものに関しては更新
-			for(int adjacent:path.get(startPoint)) {
-				int tmpDist = distance[startPoint][tmpStartPoint]+distance[tmpStartPoint][adjacent];
-				if(!fixed[adjacent] && minDistances[adjacent]>tmpDist ) {
+			for (int adjacent : path.get(startPoint)) {
+				int tmpDist = distance[startPoint][tmpStartPoint] + distance[tmpStartPoint][adjacent];
+				if (!fixed[adjacent] && minDistances[adjacent] > tmpDist) {
 					minDistances[adjacent] = tmpDist;
 					nodes.add(new Node(adjacent, tmpDist));
 				}
@@ -87,20 +139,20 @@ public class Dijkstra {
 //ノード
 class Node implements Comparable<Node> {
 
-  int point;
-  long distanceFromStart;
+	int point;
+	int distanceFromStart;
 
-  public Node(int t, long m) {
-      point = t;
-      distanceFromStart = m;
-  }
+	public Node(int t, int m) {
+		point = t;
+		distanceFromStart = m;
+	}
 
-  @Override
-  public int compareTo(Node o) {
-      int res = -1;
-      if (this.distanceFromStart - o.distanceFromStart >= 0) {
-          res = 1;
-      }
-      return res;
-  }
+	@Override
+	public int compareTo(Node o) {
+		int res = -1;
+		if (this.distanceFromStart - o.distanceFromStart >= 0) {
+			res = 1;
+		}
+		return res;
+	}
 }
