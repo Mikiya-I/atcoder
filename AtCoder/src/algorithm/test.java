02e3f7f3d74de0601942ddb5b77f89e9fcd5084f
Mikiya-I <mikiya19961012@gmail.com>
2021-10-16 20:51:55 +0900

    ダイクストラ

--- AtCoder/src/algorithm/test.java
+++ AtCoder/src/algorithm/test.java
@@ -1,25 +1,57 @@
 package algorithm;
 
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class test {
 
-	public static void main(String[] args) {
-		GridRotateSample test = new GridRotateSample();
-		for(int i=0;i<40000;i++) {
-			Random rand = new Random();
-			int x = rand.nextInt(9)+1;
-			int y = rand.nextInt(9)+1;
-			boolean[][] a = new boolean[x][y];
-			try {
-				test.rotate(a);
-			}catch (Exception e) {
-				System.out.println(x);
-				System.out.println(y);
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = reader.readLine().split(" ");
+		int h = Integer.parseInt(strs[0]);
+		int w = Integer.parseInt(strs[1]);
+		int[][] costGrid = new int[h][w];
+		//エッジ
+		Map<Integer, Set<Integer>> path = new HashMap<Integer, Set<Integer>>();
+		//コスト計算
+		for(int i=0;i<h;i++) {
+			strs = reader.readLine().split(" ");
+			for(int j=0;j<w;j++) {
+				costGrid[i][j] = Integer.parseInt(strs[j]);
 			}
 		}
-		System.out.println("ok");
+		//パス変換
+		for(int i=0;i<h;i++) {
+			for(int j=0;j<w;j++) {
+				HashSet<Integer> nodes =new HashSet<Integer>();
+				int tmpNode = w*i+j;
+				//四方のマス
+				int right= tmpNode+1;
+				int left = tmpNode-1;
+				int up = tmpNode-w;
+				int down = tmpNode+w;
+				if(right%w != 0)
+					nodes.add(right);
+				if(left > -1 && left != w-1)
+					nodes.add(left);
+				if(up >=0)
+					nodes.add(up);
+				if(down/w < h)
+					nodes.add(down);
+				path.put(w*i+j, nodes);
+			}
+		}
 		
+		Dijkstra dj = new Dijkstra(null, 0, h*w-1,h*w);
+		dj.setCost(costGrid);
+		dj.path= path;
+		
+		dj.calcMinCost();
+		System.out.println(dj.minCost[h*w-1]);
 	}
-
 }
