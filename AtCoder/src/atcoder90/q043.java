d1d03e6ff84530352ea065ae1b329a4c8aad3314
Mikiya-I <mikiya19961012@gmail.com>
2021-10-19 23:35:56 +0900

    01bfs(途中)

--- AtCoder/src/atcoder90/q043.java
+++ AtCoder/src/atcoder90/q043.java
@@ -3,8 +3,19 @@ package atcoder90;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class q043 {
+	static Map<Integer,Set<Integer>> path ;
+	static boolean[] confirmed;
+	//↑: -W;↓:W;→:1;←:-1;
+	static int vector = 0;
 	public static void main(String[] args) throws IOException {
 //		slove();
 		System.out.println(slove());
@@ -22,21 +33,66 @@ public class q043 {
 		final int y2 = Integer.parseInt(strs[0]);
 		final int x2 = Integer.parseInt(strs[1]);
 		
-		boolean[][] grid = new boolean[H+2][W+2];
-		int[][] dp = new int[H+2][W+2];
+		boolean[][] grid = new boolean[H+1][W+1];
 		//r:→ l:← u:↑ d:↓
 		char[][] vector = new char[H+2][W+2];
+		path = new HashMap<Integer, Set<Integer>>();
+		for(int i=1;i<=H*W;i++)
+			path.put(i, new HashSet<Integer>());
 		
-		for(int i=0;i<H;i++) {
+		//グリッド情報読み込み
+		for(int i=1;i<=H;i++) {
 			char[] arr = reader.readLine().toCharArray();
 			for(int j=1;j<=W;j++) {
-				if( arr[i-1] == '.')
+				if( arr[i-1] == '.') {
 					grid[i][j] = true;
+				}
+			}
+		}
+		//読み込んだグリッドからパスを通す
+		for(int i=1;i<=H;i++) {
+			for(int j=1;j<=W;j++) {
+				int current = i*W+j;
+				if(!grid[i][j])
+					continue;
+				if(grid[i+1][j]) {
+					int under = current+W;
+					path.get(current).add(under);
+					path.get(under).add(current);
+				}
+				if(grid[i][j+1]) {
+					int right = current+1;
+					path.get(current).add(right);
+					path.get(right).add(current);
+				}
 			}
 		}
 		
 		
 		
+		
 		return 0;
 	}
+}
+
+
+class BFS01_43 {
+	static ArrayDeque<Integer> que = new ArrayDeque<Integer>();
+	 void bfs(ArrayList<ArrayList<Integer>> path) {
+		 ArrayDeque<Integer> que = new ArrayDeque<Integer>();
+		    int[] dis = new int[100];
+		    Arrays.fill(dis, -1);
+		    que.push(1);
+		    dis[1] = 0;
+		    while(!que.isEmpty()){
+		        int t = que.pop();
+//		        que.pop();
+		        for(int x: path.get(t)) { 
+		        	if(dis[x] == -1) {
+			            dis[x] = dis[t] + 1;
+			            que.push(x);
+		        	}
+		        }
+		    }
+	 }
 }
