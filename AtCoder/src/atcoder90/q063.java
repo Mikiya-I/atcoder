3c9ffd36dfead2974e6a1eb93a9956fa698d6061
Mikiya-I <mikiya19961012@gmail.com>
2021-11-03 23:43:52 +0900

    途中

---
+++ AtCoder/src/atcoder90/q063.java
@@ -0,0 +1,23 @@
+package atcoder90;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q063 {
+	public static void main(String[] args) throws IOException {
+//		slove();
+		System.out.println(slove());
+	}
+	
+	public static int slove() throws  IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String[] strs = reader.readLine().split(" ");
+		final int H = Integer.parseInt(strs[0]);
+		final int W = Integer.parseInt(strs[1]);
+		
+		
+		return 0;
+	}
+}
