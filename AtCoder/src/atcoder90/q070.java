86ec46a4f0d4e318a6fe302e9e4fcb20fcbdb8c6
Mikiya-I <mikiya19961012@gmail.com>
2021-11-06 01:47:48 +0900

    昇順の数列の要素との差の最小化

--- AtCoder/src/atcoder90/q070.java
+++ AtCoder/src/atcoder90/q070.java
@@ -3,8 +3,10 @@ package atcoder90;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class q070 {
+	static long[] x,y;
 	public static void main(String[] args) throws IOException {
 //		slove();
 		System.out.println(slove());
@@ -12,54 +14,56 @@ public class q070 {
 	
 	public static long slove() throws  IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		long N = Long.parseLong(reader.readLine());
-		reader.close();
-		long ans = 0;
-		if(isPrime(N))
-			return 0;
+		final int N  = Integer.parseInt(reader.readLine());
+		x = new long[N];
+		y = new long[N];
+//		long xSum = 0;
+//		long ySum = 0;
+		for(int i=0;i<N;i++) {
+			String[] strs = reader.readLine().split(" ");
+			x[i] = Long.parseLong(strs[0]);
+			y[i] = Long.parseLong(strs[1]);
+//			xSum += x[i];
+//			ySum += y[i];
+		}
+//		double xAve = (double)xSum / N;
+//		double yAve = (double) ySum /N;
+//		long xPoint = binarySearch(xAve, x);
+//		long yPoint = binarySearch(yAve, y);
 		
-		int cnt = 0;
-		
-		long tmp = 2;
-		while(N%tmp == 0) {
-			N/=tmp;
-			cnt++;
+		Arrays.sort(x);
+		Arrays.sort(y);
+		double xPoint ;
+		double yPoint ;
+		if(N%2 == 1) {
+			xPoint =  x[N/2];
+			yPoint = y[N/2];
+		}else {
+			xPoint = (double)(x[N/2-1]+x[N/2])/2;
+			yPoint = (double)(y[N/2-1]+y[N/2])/2;
 		}
-		if(N > 1 && isPrime(N)) {
-			N =1;
-			cnt ++ ;
-		}
-		tmp ++;
-		while(N >1) {
-			if(N%tmp == 0) {
-				while(N%tmp == 0) {
-					N/=tmp;
-					cnt++;
-				}
-				if(N>1 && isPrime(N)) {
-					N = 1;
-					cnt++;
-				}
-			}
-			tmp += 2;
-		}
-		while(cnt > Math.pow(2, ans)) {
-			ans++;
+		double ans =0;
+		for(int i=0;i<N;i++) {
+			double xDistance = Math.abs(x[i] - xPoint);
+			double yDistance = Math.abs(y[i] - yPoint);
+			ans += xDistance;
+			ans += yDistance;
 		}
 		
-		return ans;
+		return (long) ans;
 	}
 	
-	private static boolean isPrime(long x) {
-		long loop = (long) Math.sqrt(x);
-		if(x%2 == 0) {
-			return x==2;
+	static long binarySearch(double base,long[] arr) {
+		int left=-1;
+		int right = arr.length;
+		while(right - left > 1) {
+			int mid = (left + right)/2;
+			if(arr[mid]>=base) {
+				right = mid;
+			}else {
+				left = mid;
+			}
 		}
-		for(int i = 3;i<=loop; i+=2) {
-			if(x%i == 0)
-				return false;
-		}
-		
-		return true;
+		return arr[right];
 	}
 }
