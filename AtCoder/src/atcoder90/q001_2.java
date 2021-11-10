ef507135a3a4b0cdd8aa4dab4eddffa047776569
Mikiya-I <mikiya19961012@gmail.com>
2021-11-10 23:54:27 +0900

    二分探索　最小値の最大化

--- AtCoder/src/atcoder90/q001_2.java
+++ AtCoder/src/atcoder90/q001_2.java
@@ -19,9 +19,63 @@ public class q001_2 {
 		strs = reader.readLine().split(" ");
 		reader.close();
 		int[] eachLength = new int[N];
-		for(int i=0;i<N;i++)
-			eachLength[i] = Integer.parseInt(strs[i]);
+		int[] points = new int[N];
+		points[0] = Integer.parseInt(strs[0]);
+		eachLength[0] = points[0];
 		
-		return 0;
+		for(int i=1;i<N;i++) {
+			points[i] = Integer.parseInt(strs[i]);
+			eachLength[i] = points[i]-points[i-1];
+		}
+//		eachLength[N-1]+=L-points[N-1];
+		
+		long ans = binarySearch(eachLength, K, L);
+		
+		return ans;
+	}
+	
+	private static long binarySearch(int[] arr,int k,int total) {
+		long ok = -1;
+		long ng = total+1;
+		while(ng - ok > 1) {
+			long mid = (ok +ng)/2;
+			if(checkPossible(arr,mid ,total,k)) {
+				ok=mid;
+			}else {
+				ng = mid;
+			}
+			
+		}
+		return ok;
+	}
+	
+	private static boolean checkPossible(int[] arr,int length,int k) {
+		int cnt =0;
+		int tmp =0;
+		for(int i:arr) {
+			if(tmp+i>=length) {
+				cnt++;
+				tmp=0;
+			}
+			else
+				tmp+=i;
+		}
+		return cnt>=k;
+	}
+	private static boolean checkPossible(int[] arr,long x,int l,int k) {
+		int sum = 0;
+		int total = 0;
+		int cnt =0;
+		for(int i=0;i<arr.length;i++) {
+			sum+= arr[i];
+			total += arr[i];
+			int min = Math.min(sum, l-total);
+			if(min >= x){
+				sum =0;
+				cnt++;
+			}
+		}
+		
+		return cnt >= k;
 	}
 }
