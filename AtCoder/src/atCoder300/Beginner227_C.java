10ab78c74832fa6f87a6dc827e8cb43af02cae79
Mikiya-I <mikiya19961012@gmail.com>
2021-11-13 22:56:56 +0900

    立方根

---
+++ AtCoder/src/atCoder300/Beginner227_C.java
@@ -0,0 +1,44 @@
+
+package atCoder300;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beginner227_C {
+	static long N;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input();
+//		slove2();
+		//		long ans = slove();
+
+		System.out.println(slove());
+	}
+
+	public static void input() throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		N = Long.parseLong(reader.readLine());
+		reader.close();
+	}
+
+	public static long slove() throws IOException {
+		long ans =0;
+		//Nの3乗根(最小値の最大値)
+		long maxA = (long) Math.cbrt(N);
+		for(int i=1;i<=maxA;i++) {
+//			if(N%i != 0)
+//				continue;
+			//Bの最大値(N/Aの√)
+			long tmp = N/i;
+			long maxB = (long) Math.sqrt(tmp);
+			for(int j=i;j<=maxB;j++) {
+				long add =tmp/j -j+1;
+				ans += add;
+			}
+		}
+		
+		return ans ;
+	}
+
+}
