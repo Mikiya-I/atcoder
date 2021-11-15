60261b94035229dda86ab4d288133b2a2cb30055
Mikiya-I <mikiya19961012@gmail.com>
2021-11-15 23:44:08 +0900

    途中

---
+++ AtCoder/src/atcoder90/q003_2.java
@@ -0,0 +1,30 @@
+package atcoder90;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.List;
+
+public class q003_2 {
+	static List<Integer>[] path;
+	static int N;
+	static boolean[] used ;
+	static ArrayDeque<Integer> que;
+	static int tmp;
+	static int maxDepth;
+	static int farest;
+	public static void main(String[] args) throws IOException {
+//		slove();
+		System.out.println(slove());
+	}
+	
+	public static long slove() throws  IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(reader.readLine());
+		String[] strs ;
+		
+		return 1;
+	}
+	
+}
