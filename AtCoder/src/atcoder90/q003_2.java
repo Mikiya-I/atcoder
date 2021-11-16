3fd43526335ba0c7d38cc1b3927e99e347ecea13
Mikiya-I <mikiya19961012@gmail.com>
2021-11-16 22:52:02 +0900

    グラフ理論:木の直径(途中)

--- AtCoder/src/atcoder90/q003_2.java
+++ AtCoder/src/atcoder90/q003_2.java
@@ -4,10 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class q003_2 {
-	static List<Integer>[] path;
+	static HashMap<Integer,HashSet<Integer>> path;
 	static int N;
 	static boolean[] used ;
 	static ArrayDeque<Integer> que;
@@ -23,8 +24,61 @@ public class q003_2 {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(reader.readLine());
 		String[] strs ;
+		path = new HashMap<Integer, HashSet<Integer>>();
+		used = new boolean[N+1];
+		//グラフ構築
+		for(int i=0;i<N-1;i++) {
+			strs = reader.readLine().split(" ");
+			int A = Integer.parseInt(strs[0]);
+			int B = Integer.parseInt(strs[1]);
+			
+			if(!path.containsKey(A)) {
+				path.put(A, new HashSet<Integer>());
+			}
+			path.get(A).add(B);
+			
+			if(!path.containsKey(B)) {
+				path.put(B, new HashSet<Integer>());
+			}
+			path.get(B).add(A);
+		}
 		
-		return 1;
+		//任意のノードから一番遠いノードを調べる
+		que = new ArrayDeque<Integer>();
+		for(int i:path.keySet()) {
+			que.add(i);
+			break;
+		}
+		//何回道を通ったか
+		tmp = 0;
+		maxDepth =0;
+		farest = -1;
+		dfs();
+		//調べたノードから一番遠いノードを調べる
+		que.add(farest);
+		maxDepth = 0;
+		dfs();
+		
+		return maxDepth;
 	}
 	
+	private static void dfs() {
+		while(que.size()>0) {
+			int tmpNode = que.poll();
+			if(used[tmpNode])
+				continue;
+			tmp++;
+			used[tmpNode] = true;
+			if(maxDepth < tmp) {
+				maxDepth = tmp;
+				farest = tmpNode;
+			}
+			for(int i:path.get(tmpNode)) {
+				que.add(i);
+				dfs();
+			}
+			used[tmpNode] = false;
+			tmp--;
+		}
+	}
 }
