886baaba97c223ee72584e75ecde0ca45df330f4
Mikiya-I <mikiya19961012@gmail.com>
2021-12-29 22:08:21 +0900

    区間和

--- AtCoder/src/atCoder400/Beginner233_D.java
+++ AtCoder/src/atCoder400/Beginner233_D.java
@@ -9,16 +9,15 @@ public class Beginner233_D {
 	static int N;
 	static long K;
 	static long[] A;
-	static int ans = 0;
+	static long ans = 0;
 
 	public static void main(String[] args) throws IOException {
 		//		long  ans = slove();
-//				slove();
+		//				slove();
 		input();
 		System.out.println(slove());
 	}
-	
-	
+
 	public static void input() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = reader.readLine().split(" ");
@@ -26,37 +25,30 @@ public class Beginner233_D {
 		K = Long.parseLong(strs[1]);
 		A = new long[N];
 		strs = reader.readLine().split(" ");
-		for(int i=0;i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			A[i] = Long.parseLong(strs[i]);
 		}
-		
+
 		reader.close();
 	}
-	
-	public static int slove() throws IOException {
-		long[] sums = new long[N+1];
-		HashMap<Long,Integer> map = new  HashMap<Long, Integer>();
+
+	public static long slove() throws IOException {
+		long[] sums = new long[N + 1];
+		HashMap<Long, Integer> map = new HashMap<Long, Integer>();
 		//初期値(最初)
 		map.put((long) 0, 1);
-		for(int i=1;i<N;i++) {
-			long tmp = A[i-1]+sums[i-1];
+		for (int i = 1; i < N + 1; i++) {
+			long tmp = A[i - 1] + sums[i - 1];
 			sums[i] = tmp;
-			if(!map.containsKey(tmp)) {
-				map.put(tmp, 0);
-			}
-			map.replace(tmp, map.get(tmp)+1);
 		}
-		
-		for(int i=0;i<N+1;i++) {
-			long tmp = sums[i];
-			map.replace(tmp, map.get(tmp)-1);
-			if(map.containsKey(K+tmp)) {
-				ans += map.get(K-tmp);
+
+		for (int i = 1; i <= N; i++) {
+			long x = sums[i] - K;
+			if (map.containsKey(x)) {
+				ans += map.get(x);
 			}
+			map.put(sums[i], map.getOrDefault(sums[i], 0) + 1);
 		}
-		
 		return ans;
 	}
-	
 }
-
