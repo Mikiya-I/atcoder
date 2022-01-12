a5f5d689bc305afc9b426f4dfb8ed20f627a88c2
Mikiya-I <mikiya19961012@gmail.com>
2022-01-12 20:16:34 +0900

    bit全探索

--- AtCoder/src/atcoder90/q063_2.java
+++ AtCoder/src/atcoder90/q063_2.java
@@ -3,63 +3,75 @@ package atcoder90;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class q063_2 {
+	
+	static int[][] grid;
+	static int H,W;
 	public static void main(String[] args) throws IOException {
 //		slove();
+		input();
 		System.out.println(slove());
 	}
 	
-	public static long slove() throws  IOException {
+	static void input() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = reader.readLine().split(" ");
-		long n = Long.parseLong(strs[0]);
-		long K = Long.parseLong(strs[1]);
-		reader.close();
-		int cnt = 0;
-		boolean[] used = new boolean[100001];
-		ArrayDeque<Long> que = new ArrayDeque<Long>();
-		//K回行うかループに入ったら終了
-		while(cnt <K) {
-			n = buttonA(n);
-			if(used[(int) n])
-				break;
-			que.add(n);
-			used[(int) n] = true;
-			cnt ++;
-		}
-		//K回行った場合は最後の要素を返す
-		if(cnt == K)
-			return que.getLast();
-		
-		//ループに入るまでの回数を数える(残ったqueのsizeがループ部分の大きさ)
-		while(que.size()>0) {
-			if(n == que.peek())
-				break;
-			else {
-				que.pop();
-				K--;
+		H = Integer.parseInt(strs[0]);
+		W = Integer.parseInt(strs[1]);
+		grid = new int[H][W];
+		for(int i=0;i<H;i++) {
+			strs = reader.readLine().split(" ");
+			for(int j=0;j<W;j++) {
+				grid[i][j] = Integer.parseInt(strs[j]);
 			}
 		}
-		long remain = K%que.size();
-		if(remain == 0) {
-			return que.getLast();
-		}
-		for(int i=0;i<remain-1;i++) {
-			que.pop();
-		}
-		
-		return que.pop();
+		reader.close();
 	}
 	
-	static long buttonA(long x) {
-		long y = 0;
-		long originX = x;
-		while(x>0) {
-			y += x%10;
-			x/= 10;
+	public static long slove() throws  IOException {
+		long ans = 1;
+		int bitPattern = 1<<H;
+		for(int i=1;i<bitPattern;i++) {
+			long tmpCnt = 0;
+			int rows = 0;
+			boolean[] flag = new boolean[H];
+			//桁分の繰り返し
+	    	for(int j=0;j<H;j++) {
+	    		//ビットが立っている場所を特定
+	    		if(((i>>j) & 1)==1)  {
+	    			flag[j] = true; 
+	    			rows++;
+	    		}
+	    	}
+	    	Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+	    	for(int w = 0;w<W;w++) {
+	    		int firstNum = 0;
+	    		boolean ok = true;
+	    		for(int h = 0;h<H;h++) {
+	    			if(flag[h] && firstNum != grid[h][w]) {
+	    				if(firstNum==0) {
+	    					firstNum = grid[h][w];
+	    				}else {
+	    					ok = false;
+	    					break;
+	    				}
+	    			}
+	    		}
+	    		if(!ok)
+	    			continue;
+	    		if(!map.containsKey(firstNum)) {
+	    			map.put(firstNum, 0);
+	    		}
+	    		int tmp = map.get(firstNum)+rows;
+	    		tmpCnt = Math.max(tmpCnt, tmp);
+	    		map.replace(firstNum, tmp);
+	    	}
+	    	ans = Math.max(ans, tmpCnt);
 		}
-		return (originX+y)%100000;
+		return ans;
 	}
+	
 }
