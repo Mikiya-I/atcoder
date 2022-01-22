10f561954c7a1950318218b6a2fb95f4d68e9248
Mikiya-I <mikiya19961012@gmail.com>
2022-01-22 23:51:09 +0900

    途中

--- AtCoder/src/atcoder90/q084_2.java
+++ AtCoder/src/atcoder90/q084_2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class q084_2 {
-	static int N;
+	static int N,oCnt;
 	static boolean[] arr;
 	public static void main(String[] args) throws IOException {
 		input();
@@ -18,16 +18,52 @@ public class q084_2 {
 		N = Integer.parseInt(reader.readLine());
 		char[] s = reader.readLine().toCharArray();
 		arr = new boolean[N];
+		oCnt = 0;
 		for(int i=0;i<N;i++) {
 			arr[i] = s[i]=='o';
+			if(arr[i])
+				oCnt++;
 		}
 		reader.close();
 	}
 	
 	public static long slove() throws  IOException {
+		//Lを固定し、条件を満たす一番小さいRを探す
+		int[] oIdxes = new int[oCnt];
+		int[] xIdxes = new int[N-oCnt];
+		if(oIdxes.length * xIdxes.length == 0) {
+			return 0;
+		}
+		int oIdx = 0;
+		int xIdx = 0;
+		for(int i=0;i<N;i++) {
+			if(arr[i]) {
+				oIdxes[oIdx] = i;
+				oIdx ++;
+			}else {
+				xIdxes[xIdx]=i;
+				xIdx++;
+			}
+		}
+		long ans =0;
+		int oTmpIdx = 0;
+		int xTmpIdx =0;
+		for(int i=0;i<N;i++) {
+			//Lがoの場合
+			if(arr[i]) {
+				while(xIdxes[xTmpIdx]< i || xTmpIdx< xIdxes[ N-oCnt]) {
+					xTmpIdx ++;
+				}if() {
+					
+				}
+			}
+			//Lがxの場合
+			else {
+				
+			}
+		}
 		
-		
-		return 0;
+		return ans;
 	}
 	
 }
