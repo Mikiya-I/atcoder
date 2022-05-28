a5c8a86a79c8f908d9118d3e6e389e80ac3b9cfe
Mikiya-I <65333537+Mikiya-I@users.noreply.github.com>
2022-05-28 23:07:06 +0900

    最小公倍数

---
+++ AtCoder/src/algorithm/LeastCommonMutiple.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class LeastCommonMutiple {
+	static long lcm (long a, long b) {
+		long temp;
+		long c = a;
+		c *= b;
+		while((temp = a%b)!=0) {
+			a = b;
+			b = temp;
+		}
+		return c/b;
+	}
+}
